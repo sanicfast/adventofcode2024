8ba2ed90ee4483e97e619d6fcbe0b9c17c311d05
sanicfast <sanic.go.fast.step.it.up@gmail.com>
2025-11-09 13:03:39 -0600

    day 6 implemented floyd fast and slow alg. 100ms!

--- day06/day06.go
+++ day06/day06.go
@@ -73,7 +73,7 @@ func timeTick(guard *Guard, obstacles [][]rune, dim Coord) int {
 	newRow := guard.heading.row + guard.location.row
 	newCol := guard.heading.col + guard.location.col
 
-	// test bounds. 1 means the guard left.
+	// test bounds. 1 means the guard escaped.
 	if newRow < 0 || newRow >= dim.row {
 		return 1
 	} else if newCol < 0 || newCol >= dim.row {
@@ -165,14 +165,6 @@ func copyGuardMap(original map[Guard]struct{}) map[Guard]struct{} {
 	}
 	return newMap
 }
-func copyCoordMap(original map[Coord]struct{}) map[Coord]struct{} {
-	newMap := make(map[Coord]struct{}, len(original))
-
-	for key, val := range original {
-		newMap[key] = val
-	}
-	return newMap
-}
 
 func countLoopsSmarter(guard Guard, obstacles [][]rune, dim Coord) int {
 	initialGuardCoord := guard.location
@@ -218,6 +210,86 @@ func countLoopsSmarter(guard Guard, obstacles [][]rune, dim Coord) int {
 	return sum
 }
 
+func detectLoopFloyd(guard Guard, obstacles [][]rune, dim Coord) int {
+	fast := guard
+	slow := guard
+	for {
+		if timeTick(&fast, obstacles, dim) == 1 {
+			return 0
+		}
+		if timeTick(&fast, obstacles, dim) == 1 {
+			return 0
+		}
+		if timeTick(&slow, obstacles, dim) == 1 {
+			return 0
+		}
+		if fast == slow {
+			break
+		}
+	}
+	return 1
+}
+
+func countLoopsFloyd(guard Guard, obstacles [][]rune, dim Coord) int {
+	initialGuardCoord := guard.location
+	sum := 0
+	finished := 0
+	visitedLocations := make(map[Coord]struct{})
+
+	// while haven't finished
+	for finished == 0 {
+		// drop an obstacle in front of the guard and see if it created a loop
+		newObstacle := Coord{
+			guard.location.row + guard.heading.row,
+			guard.location.col + guard.heading.col}
+
+		_, alreadyTried := visitedLocations[newObstacle]
+
+		if newObstacle.row < 0 || newObstacle.row >= dim.row {
+			break
+		} else if newObstacle.col < 0 || newObstacle.col >= dim.row {
+			break
+		}
+
+		o := obstacles[newObstacle.row][newObstacle.col]
+
+		if !alreadyTried && newObstacle != initialGuardCoord && o != '#' {
+			obstacles[newObstacle.row][newObstacle.col] = '#'
+			loopDetected := detectLoopFloyd(guard, obstacles, dim)
+			sum += loopDetected
+			obstacles[newObstacle.row][newObstacle.col] = '.'
+		}
+		// add it to our list of places we've been
+		visitedLocations[guard.location] = struct{}{}
+		// add this guard state to our dictionary
+		// check if we've finished and if not move forward a tick
+		finished = timeTick(&guard, obstacles, dim)
+	}
+	return sum
+}
+
+func countLoopsFloydBruteForce(guard Guard, obstacles [][]rune, dim Coord) int {
+	initialGuardCoord := guard.location
+	sum := 0
+	for i := range dim.row {
+		for j := range dim.col {
+			obstacleLocation := Coord{i, j}
+			if obstacleLocation == initialGuardCoord {
+				continue
+			}
+			o := obstacles[i][j]
+			if o != '#' {
+				obstacles[i][j] = '#'
+
+				loopDetected := detectLoopFloyd(guard, obstacles, dim)
+				sum += loopDetected
+				obstacles[i][j] = '.'
+			}
+		}
+	}
+	return sum
+}
+
 func main() {
 	startTime := time.Now()
 	guard, obstacles, dim := readInput()
@@ -225,11 +297,15 @@ func main() {
 	part1 := countGuardLocations(guard, obstacles, dim)
 	fmt.Println("Day 6, Part 1:", part1)
 
-	// part2 := countLoopsBruteForce(guard, obstacles, dim) // 2m15s for brute force! 36s if we put the obstacles in a map
+	// part2_1 := countLoopsBruteForce(guard, obstacles, dim) // 2m15s for brute force! 36s if we put the obstacles in a map
 	// fmt.Println("Day 6, Part 2:", part2)
 
-	part2_2 := countLoopsSmarter(guard, obstacles, dim)
-	fmt.Println("Day 6, Part 2:", part2_2)
+	// part2_2 := countLoopsSmarter(guard, obstacles, dim) //5-8 seconds
+	// fmt.Println("Day 6, Part 2:", part2_2)
+	part2_3 := countLoopsFloyd(guard, obstacles, dim) //100 ms!
+	fmt.Println("Day 6, Part 2:", part2_3)
+	// part2_4 := countLoopsFloydBruteForce(guard, obstacles, dim) // 754 ms!
+	// fmt.Println("Day 6, Part 2:", part2_4)
 
 	fmt.Println(time.Since(startTime))
 
